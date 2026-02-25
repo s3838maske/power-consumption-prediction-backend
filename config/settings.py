ff9d0ed6d8238c0f6382085e2e8c97fe1dc3b5bd
shubham maske <124877144+s3838maske@users.noreply.github.com>
2026-02-25 19:11:10 +0530

    Add new CORS origin for production URL

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://power-consumption-ai-prediction.vercel.app/"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
